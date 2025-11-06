f267bd6597a518a62fbfa42c141c9bcde0ef38ab
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-11-06 09:58:18 +0700

    feat: configuration

---
+++ prescal/include/configuration.h
@@ -0,0 +1,19 @@
+#ifndef CONFIGURATION
+#define CONFIGURATION
+
+#include <stdint.h>
+#include "./ds/linkedlist.h"
+
+#define PATH "/home/keyzarrasya/Documents/project/prescal/prescal/config.yml"
+
+struct prescal_config{
+    uint16_t port;
+    char *entry;
+    struct linkedlist *forwards;
+};
+
+struct prescal_config *config_init(void);
+void read_config(struct prescal_config *config);
+void print_config(struct prescal_config *config);
+
+#endif
