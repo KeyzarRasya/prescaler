19c2e12e44538e2cbb5822e1782c7c8bfbdd018d
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-11-11 22:28:00 +0700

    feat: adjsutment

--- traffic/main.c
+++ traffic/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "include/http.h"
+#include "include/helper.h"
+
+void test_read_env() {
+    char token[256];
+    read_env("TSDB_TOKEN", token, sizeof(token));
+
+    printf("%s", token);
+}
 
 int main() {
-    traffic_worker();
+    test_read_env();
     return 0;
 }
