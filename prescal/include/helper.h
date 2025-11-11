214f1eeec8c6a04332ac2b08e07c941f71ceb0f4
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-11-11 22:26:49 +0700

    feat: changing .env path

--- prescal/include/helper.h
+++ prescal/include/helper.h
@@ -1,7 +1,10 @@
+
 #ifndef HELPER
 #define HELPER
 
-int read_env(const char *to_seach, char *dest);
+#include <stddef.h>
+
+int read_env(const char *to_seach, char *dest, size_t dest_size);
 int split_equal(const char *input, char **left, char **right);
 
 #endif
