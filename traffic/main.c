380a102e99020ebc0e1e027982094672237e5386
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-11-14 01:05:03 +0700

    feat: traffic worker

--- traffic/main.c
+++ traffic/main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 #include "include/http.h"
-#include "include/helper.h"
 
-void test_read_env() {
-    char token[256];
-    read_env("TSDB_TOKEN", token, sizeof(token));
-
-    printf("%s", token);
-}
 
 int main() {
-    test_read_env();
+    traffic_worker();
     return 0;
 }
