3169f99e7ba8a0a4b89b0e7e091283c5b5d6fb0b
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-12-24 00:15:51 +0700

    feat: flexibles traffic

--- traffic/include/http.h
+++ traffic/include/http.h
@@ -1,11 +1,16 @@
 #ifndef HTTP
 #define HTTP
 
+#include "config.h"
+#include <time.h>
+
 #define MAX_BUFFER_SIZE 9000
 #define HOST "localhost:80"
 
 void prescal_traffic();
 void send_request();
 void traffic_worker();
+int traffic_amount_v2(struct tm *localtime, traffic_config_t *config);
+int is_in_period(int hour, time_period_t *period);
 
 #endif
